841edf981a54b2efb7bd301102f90922d9768cb4
Youssef Abdel Mottaleb <114946204+Youssef-AbdelMottaleb@users.noreply.github.com>
2022-12-24 01:34:01 +0200

    Add files via upload

---
+++ Prim's Algorithim/Prim's Algorithim/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prim_s_Algorithim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+			int[,] graph = {
+								{ 4, 1, 3, 2, 7 },
+								{ 2, 0, 3, 8, 5 },
+								{ 2, 3, 2, 0, 7 },
+								{ 0, 5, 7, 9, 0 },
+								{ 0, 2, 1, 4, 9 },
+							};
+			Prim(graph, 5);
+			Console.ReadKey();
+		}
+
+		private static int MinKey(int[] key, bool[] set, int numberOfVertices)
+		{
+			// Setting all Nodes to Maximum Values
+			int min = int.MaxValue, minIndex = 0;
+
+			for (int v = 0; v < numberOfVertices; ++v)
+			{
+				if (set[v] == false && key[v] < min)
+				{
+					min = key[v];
+					minIndex = v;
+				}
+			}
+
+			return minIndex;
+		}
+
+		private static void displayResults(int[] parent, int[,] graph, int numberOfVertices)
+		{
+			Console.WriteLine("\t  Edge\t\t         Weight \n");
+			// Number Of Edges in MST is : E = V - 1
+			for (int i = 1; i < numberOfVertices; ++i)
+				Console.WriteLine("from vertex {0} -  to vertex {1}  the Weight is  {2}", parent[i], i, graph[i, parent[i]]);
+		}
+
+		public static void Prim(int[,] graph, int numberOfVertices)
+		{
+			int[] parent = new int[numberOfVertices];
+			int[] key = new int[numberOfVertices];
+			bool[] mstSet = new bool[numberOfVertices];
+
+			for (int i = 0; i < numberOfVertices; ++i)
+			{
+				key[i] = int.MaxValue;
+				mstSet[i] = false;
+			}
+
+			key[0] = 0;
+			parent[0] = -1;
+
+			for (int count = 0; count < numberOfVertices - 1; ++count)
+			{
+				int u = MinKey(key, mstSet, numberOfVertices);
+				mstSet[u] = true;
+
+				for (int v = 0; v < numberOfVertices; ++v)
+				{
+				    // weight = 0 means vertex to itself which must be cancelled so we converted to bool
+					if (Convert.ToBoolean(graph[u, v]) && mstSet[v] == false && graph[u, v] < key[v])
+					{
+						parent[v] = u;
+						key[v] = graph[u, v];
+					}
+				}
+			}
+			displayResults(parent, graph, numberOfVertices);
+		}
+	}
+}
---
+++ Prim's Algorithim/Prim's Algorithim/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Prim's Algorithim")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prim's Algorithim")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("201107dc-0c2b-4d0e-a364-7e9c17cd9d2a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
